fd920619d22acc87e9c936b5b643b2881eff8230
Felipe Borges Caetano <caetano.fbc@gmail.com>
2020-12-04 14:03:14 -0300

    finalize login and cadastro methods

--- blogpessoal-ui/src/app/cadastro/cadastro.component.ts
+++ blogpessoal-ui/src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User()
+  senha: string
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
   }
 
+  cadastrar() {
+    if ( this.senha === this.user.senha ) {
+      this.authService.cadastrar(this.user).subscribe((resp: User) => {
+        this.user = resp
+        this.router.navigate(['/login'])
+        alert('Usuário cadastrado com sucesso!')
+      })
+    } else {
+      alert('Senha inválida!')
+    }
+  }
+
+  conferirSenha(event: any) {
+    this.senha = event.target.value
+  }
 }
--- blogpessoal-ui/src/app/service/auth.service.ts
+++ blogpessoal-ui/src/app/service/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -10,11 +11,33 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
-  logar(userLogin: UserLogin) {
-    return this.http.post('http://localhost:9000/usuarios/logar', userLogin)
+  logar(userLogin: UserLogin) : Observable<UserLogin> {
+    return this.http.post<UserLogin>('http://localhost:9000/usuarios/logar', userLogin)
   }
 
-  cadastrar(user: User) {
-    return this.http.post('http://localhost:9000/usuarios/cadastrar', user)
+  cadastrar(user: User) : Observable<User> {
+    return this.http.post<User>('http://localhost:9000/usuarios/cadastrar', user)
+  }
+
+  btnSair() {
+    let ok = false
+    let token = localStorage.getItem('token')
+
+    if (token != null) {
+      ok = true
+    }
+
+    return ok
+  }
+
+  btnLogin() {
+    let ok = false
+    let token = localStorage.getItem('token')
+
+    if (token == null) {
+      ok = true
+    }
+
+    return ok
   }
 }
